f59dbb606fd216e4c030e0819ac2460121ee5efd
eduardagoulart <goularteduarda.a@gmail.com>
2019-08-03 19:06:58 -0300

    FEATURE: searching given months as parameters

--- data_project/core/views.py
+++ data_project/core/views.py
@@ -1,5 +1,7 @@
-from data_project.core.models import Insurance
+from rest_framework import generics
 from rest_framework import viewsets
+
+from data_project.core.models import Insurance
 from data_project.core.serializers import InsuranceSerializer
 
 
@@ -9,3 +11,18 @@ class InsuranceViewSet(viewsets.ModelViewSet):
     """
     queryset = Insurance.objects.all()
     serializer_class = InsuranceSerializer
+
+
+class PurchaseList(generics.ListAPIView):
+    serializer_class = InsuranceSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Insurance.objects.all()
+        username = self.request.query_params.get('state_abbr', None)
+        if username is not None:
+            queryset = queryset.filter(months=username)
+        return queryset
