447445becbc5d6d4273f9d64ecd0da5b67b73ea5
eduardagoulart <goularteduarda.a@gmail.com>
2019-08-05 13:51:01 -0300

    FEATURE: made View from csv using Django-Rest-Pandas

--- data_project/core/views.py
+++ data_project/core/views.py
@@ -1,14 +1,12 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_pandas import PandasView, PandasCSVRenderer
 
 from data_project.core.models import Insurance
 from data_project.core.serializers import InsuranceSerializer
 
 
 class InsuranceViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
     queryset = Insurance.objects.all()
     serializer_class = InsuranceSerializer
 
@@ -17,10 +15,6 @@ class PurchaseList(generics.ListAPIView):
     serializer_class = InsuranceSerializer
 
     def get_queryset(self):
-        """
-        Optionally restricts the returned purchases to a given user,
-        by filtering against a `username` query parameter in the URL.
-        """
         queryset = Insurance.objects.all()
         generic_request = self.request.query_params.get(list(self.request.query_params.keys())[0], None)
 
@@ -33,4 +27,41 @@ class PurchaseList(generics.ListAPIView):
                 queryset = queryset.filter(agency_id=generic_request)
             elif list(self.request.query_params.keys())[0] == 'stat_profile_date_year':
                 queryset = queryset.filter(stat_profile_date_year=generic_request)
+
         return queryset
+
+
+class TimeSeriesView(PandasView):
+    queryset = Insurance.objects.all()
+
+    def filter_queryset(self, queryset):
+        queryset = Insurance.objects.all()
+        generic_queryset = self.request.query_params.get('data', None)
+
+        if generic_queryset is not None:
+            queryset = queryset.filter(stat_profile_date_year=generic_queryset)
+        return queryset
+
+    serializer_class = InsuranceSerializer
+
+    def transform_dataframe(self, dataframe):
+        dataframe = dataframe.drop(
+            ['primary_agency_id', 'prod_abbr', 'state_abbr', 'stat_profile_date_year', 'retention_poly_qty',
+             'poly_inforce_qty', 'prev_poly_inforce_qty', 'nb_wrtn_prem_amt', 'wrtn_prem_amt', 'prev_wrtn_prem_amt',
+             'prd_ernd_prem_amt', 'prd_incrd_losses_amt', 'months', 'retention_ratio', 'loss_ratio', 'loss_ratio_3yr',
+             'growth_rate_3yr', 'agency_appointment_year', 'active_producers', 'max_age', 'min_age', 'vendor_ind',
+             'vendor', 'pl_start_year', 'pl_end_year', 'commisions_start_year', 'commisions_end_year', 'cl_start_year',
+             'cl_end_year', 'activity_notes_start_year', 'activity_notes_end_year', 'cl_bound_ct_mds', 'cl_quo_ct_mds',
+             'cl_bound_ct_sbz', 'cl_quo_ct_sbz', 'cl_bound_ct_eqt', 'cl_quo_ct_eqt', 'pl_bound_ct_elinks',
+             'pl_quo_ct_elinks', 'pl_bound_ct_plrank', 'pl_quo_ct_plrank', 'pl_bound_ct_eqtte', 'pl_quo_ct_eqtte',
+             'pl_bound_ct_applied', 'pl_quo_ct_applied', 'pl_bound_ct_transactnow', 'pl_quo_ct_transactnow'], axis=1)
+
+        return dataframe
+
+    renderer_classes = [PandasCSVRenderer]
+
+    def get_pandas_filename(self, request, format):
+        if format in ('csv', 'xls', 'xlsx'):
+            return "report_data"
+        else:
+            return None
