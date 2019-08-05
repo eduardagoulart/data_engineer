4fbf4ddc90bfa5a3c9474e80c1ba8039f5aa4b08
eduardagoulart <goularteduarda.a@gmail.com>
2019-08-05 13:50:15 -0300

    FEATURE: adding url path to export csv

--- data_project/urls.py
+++ data_project/urls.py
@@ -22,13 +22,13 @@ from data_project.core import views
 router = routers.DefaultRouter()
 router.register(r'insurance', views.InsuranceViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-
 urlpatterns = [
 
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
     path('purshcase/', views.PurchaseList.as_view()),
+    path('report/', views.TimeSeriesView.as_view()),
 ]
+
+
