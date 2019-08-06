fdb001273aa0a02119cb81624545b0053bb21edd
eduardagoulart <goularteduarda.a@gmail.com>
2019-08-06 10:14:55 -0300

    FEATURE: creating the serializer

--- data_project/core/serializers.py
+++ data_project/core/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
-from data_project.core.models import Insurance
+from data_project.core.models import Insurance, Months, Year, State, AgencyId
 
 
 class InsuranceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Insurance
+        fields = '__all__'
+
+
+class MonthsSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Months
+        fields = '__all__'
+
+
+class YearSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Year
+        fields = '__all__'
+
+
+class StateSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = State
+        fields = '__all__'
+
+
+class AgencyIdSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = AgencyId
         fields = '__all__'
