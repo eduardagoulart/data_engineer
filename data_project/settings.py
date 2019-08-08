fe76d8ec57801d1d2cd4469f6ac7e0e026ff7043
eduardagoulart <goularteduarda.a@gmail.com>
2019-08-08 09:44:56 -0300

    UPDATE: removing dj-database-url

--- data_project/settings.py
+++ data_project/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+
 from decouple import config
-from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,12 +84,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-'''
 
-DATABASES = {
-    'default': config('DATABASE_URL', cast=dburl)
-}
-'''
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
