d75f6abcc81947eb7836104929a2c80577390f2b
eduardagoulart <goularteduarda.a@gmail.com>
2019-08-08 09:38:01 -0300

    UPDATE: url allowed

--- data_project/settings.py
+++ data_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'britecoretest.ibwmngzn52.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'britecore.p5m92wre9i.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
